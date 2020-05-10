fb1ac33b068c5fea9b2074c0d72025cc84154cfb
Mateus Braun Vizotto <mateusbraunvizotto@gmail.com>
2020-05-09 22:14:28 -0300

    widget

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:shopping/pages/product.page.dart';
+import 'package:shopping/widgets/category/category-list.widget.dart';
+import 'package:shopping/widgets/product/product-list.widget.dart';
+import 'package:shopping/widgets/search-box.widget.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -15,7 +17,7 @@ class HomePage extends StatelessWidget {
               SizedBox(
                 height: 60,
               ),
-              search(),
+              SearchBox(),
               SizedBox(
                 height: 30,
               ),
@@ -30,7 +32,7 @@ class HomePage extends StatelessWidget {
               ),
               Container(
                 height: 90,
-                child: categoryList(),
+                child: CategoryList(),
               ),
               SizedBox(
                 height: 30,
@@ -55,7 +57,9 @@ class HomePage extends StatelessWidget {
               ),
               Container(
                 height: 350,
-                child: productList(context),
+                child: ProductList(
+                  scrollDirection: Axis.horizontal,
+                ),
               ),
             ],
           ),
@@ -63,163 +67,4 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
-}
-
-Widget search() {
-  return Container(
-    height: 60,
-    padding: EdgeInsets.only(
-      left: 20,
-    ),
-    decoration: BoxDecoration(
-      color: Colors.black.withOpacity(0.1),
-      borderRadius: BorderRadius.all(
-        Radius.circular(128),
-      ),
-    ),
-    child: Row(
-      children: <Widget>[
-        Icon(Icons.search),
-        Container(
-          width: 300,
-          padding: EdgeInsets.only(left: 10),
-          child: TextFormField(
-            keyboardType: TextInputType.text,
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              labelText: "Search...",
-              labelStyle: TextStyle(
-                color: Colors.blue,
-                fontWeight: FontWeight.w300,
-                fontSize: 16,
-              ),
-            ),
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.blue,
-            ),
-          ),
-        ),
-      ],
-    ),
-  );
-}
-
-Widget categoryList() {
-  return Container(
-    child: ListView(
-      scrollDirection: Axis.horizontal,
-      children: <Widget>[
-        categoryItem(),
-        categoryItem(),
-        categoryItem(),
-        categoryItem(),
-        categoryItem(),
-        categoryItem(),
-        categoryItem(),
-        categoryItem(),
-      ],
-    ),
-  );
-}
-
-Widget categoryItem() {
-  return Container(
-    width: 70,
-    height: 70,
-    margin: EdgeInsets.all(10),
-    padding: EdgeInsets.all(10),
-    decoration: BoxDecoration(
-      color: Colors.white,
-      boxShadow: [
-        new BoxShadow(
-          color: Colors.black12,
-          offset: new Offset(1, 1),
-          blurRadius: 5,
-          spreadRadius: 2,
-        )
-      ],
-      borderRadius: BorderRadius.all(
-        Radius.circular(64),
-      ),
-    ),
-    child: Image.asset("assets/Icon_Devices.png"),
-  );
-}
-
-Widget productList(BuildContext context) {
-  return Container(
-    child: ListView(
-      scrollDirection: Axis.horizontal,
-      children: <Widget>[
-        productItem(context),
-        productItem(context),
-        productItem(context),
-        productItem(context),
-      ],
-    ),
-  );
-}
-
-Widget productItem(BuildContext context) {
-  return Container(
-    padding: EdgeInsets.all(10),
-    margin: EdgeInsets.all(5),
-    width: 170,
-    color: Colors.black12,
-    child: Column(
-      children: <Widget>[
-        GestureDetector(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => ProductPage(),
-              ),
-            );
-          },
-          child: Image.asset(
-            "assets/product-1.png",
-            width: 170,
-            height: 170,
-            fit: BoxFit.cover,
-          ),
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        Container(
-          height: 60,
-          child: Text(
-            "Titulo do produto",
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.w300,
-            ),
-          ),
-        ),
-        SizedBox(
-          height: 5,
-        ),
-        Text(
-          "Marca",
-          style: TextStyle(
-            fontSize: 14,
-            fontWeight: FontWeight.w300,
-          ),
-        ),
-        SizedBox(
-          height: 5,
-        ),
-        Text(
-          "\$ 200",
-          style: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-            color: Color(0xFF00C569),
-          ),
-        ),
-      ],
-    ),
-  );
 }
